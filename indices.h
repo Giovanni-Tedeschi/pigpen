d5fa4ec49d7e4b5197f674ed55235648298c35a0
giovanni-tedeschi <giov.tedeschi@gmail.com>
2025-10-11 13:05:42 +0200

    Clean-up of the tests

---
+++ indices.h
@@ -0,0 +1,15 @@
+#ifndef INDICES_H
+#define INDICES_H
+
+struct Indices {
+    int rho;
+    int vx;
+    int vy;
+    int vz;
+    int P;
+    // Add more as needed
+};
+
+extern Indices idx; // Declare global variable
+
+#endif // INDICES_H
